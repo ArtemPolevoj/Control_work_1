9cb8989146ae7fb9ffa910bc24be0431317aa48d
Артем <aretmpolevoj@yandex.ru>
2023-02-13 12:18:51 +0300

    Added void getArrayFromUser

--- src/ArrayString.java
+++ src/ArrayString.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class ArrayString {
+    public static String[] getArrayFromUser(){
+        System.out.println("Enter lines separated by commas.");
+        Scanner scanner = new Scanner(System.in);
+        String worlds = scanner.nextLine();
+        String[] temp = worlds.split(",");
+        String[] inpurArray = new String[temp.length];
+        for (int i = 0; i < temp.length; i++) {
+            inpurArray[i] = temp[i].trim();
+        }
+    return inpurArray;
+    }
 }
